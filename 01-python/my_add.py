d397b40114272764586bda5493c6bb9dcaadbd10
jayshenn <jiuyang1215@gmail.com>
2025-11-11 17:27:54 +0800

    Rename directory to 01-python and add modules/packages and advanced syntax notebooks

---
+++ 01-python/my_add.py
@@ -0,0 +1,11 @@
+__all__ = ["num", "add"]
+
+num = 100
+num1 = 200
+_str1 = "abc"
+
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print(add(10, 20))
---
+++ 01-python/my_multi.py
@@ -0,0 +1,5 @@
+num = 200
+
+def multi(a, b):
+    """求两个数的乘积"""
+    return a * b
